44e18687650b1149ec06b0ce86367975372bd463
AnderssonPeter <1308309+AnderssonPeter@users.noreply.github.com>
2022-01-05 17:05:02 +0100

    Added workflow to create a base dictionary for git

---
+++ generate dictionaries/git/asciidoc.to.dictionary.ps1
@@ -0,0 +1,30 @@
+$files = ls *.xml
+$transformationFile = Join-Path $PSScriptRoot "asciidoc.to.dictionary.xlts"
+$xslt = New-Object System.Xml.Xsl.XslCompiledTransform
+
+Write-Host "Loading transformation file"
+$xsltSettings = New-Object System.Xml.Xsl.XsltSettings($true, $true)
+$xsltResolver = New-Object System.Xml.XmlUrlResolver
+$xslt.Load($transformationFile, $xsltSettings, $xsltResolver)
+$result = ""
+foreach ($file in $files)
+{
+	Write-Host "Processing $file"
+	$inputStream = [System.IO.File]::OpenRead($file)
+	$reader = New-Object System.Xml.XmlTextReader($inputStream)
+	$reader.DtdProcessing = [System.Xml.DtdProcessing]::Ignore
+	
+	$outputStream = New-Object System.IO.MemoryStream
+	$writer = New-Object System.Xml.XmlTextWriter($outputStream, [System.Text.Encoding]::UTF8)
+	Write-Host "Transforming"
+	$xslt.Transform($reader, $writer)
+	$writer.Flush();
+	$content = $outputStream.ToArray()
+	$contentString = [System.Text.Encoding]::UTF8.GetString($content);
+	$result += $contentString + "`n"
+	$outputStream.Dispose()
+	$inputStream.Dispose()
+}
+
+Write-Host "Writing"
+Set-Content "output.ps1" $result
