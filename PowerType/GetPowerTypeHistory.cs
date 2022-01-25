8051667b9faaab0213d145351c1d2b396b1981cf
AnderssonPeter <1308309+AnderssonPeter@users.noreply.github.com>
2022-01-25 17:42:49 +0100

    Added missing file and OnlyErrors

--- PowerType/GetPowerTypeHistory.cs
+++ PowerType/GetPowerTypeHistory.cs
@@ -9,6 +9,12 @@ namespace PowerType;
 [Cmdlet("Get", "PowerTypeHistory"), OutputType(typeof(bool))]
 public class GetPowerTypeHistory : PowerTypeCmdlet
 {
+    /// <summary>
+    /// <para type="description">Indicates whether the user would like to receive output. </para>
+    /// </summary>
+    [Parameter(Mandatory = false, HelpMessage = "Only return rows with exceptions")]
+    public SwitchParameter OnlyErrors { get; set; }
+
     /// <inheritdoc/>
     protected override void ProcessRecord()
     {
@@ -17,7 +23,7 @@ public class GetPowerTypeHistory : PowerTypeCmdlet
         {
             throw new InvalidOperationException("Please call 'Enable-PowerTypePredictor' first");
         }
-        this.WriteObject(instance.PredictionSummaryCollector.Get());
+        this.WriteObject(instance.PredictionSummaryCollector.Get().Where(x => !OnlyErrors.IsPresent || x.Exception != null));
     }
 }
 
---
+++ PowerType/Model/DictionaryState.cs
@@ -0,0 +1,10 @@
+﻿namespace PowerType.Model;
+
+[Flags]
+public enum DictionaryState
+{
+    Unknown = 0,
+    Generated = 1,
+    Experimental = 2,
+    Incomplete = 4
+}
